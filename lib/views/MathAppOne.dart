185dba808be33c8be90e4c55e4b1464232d2f861
Hazlan Qodrey <hazlanmuhammadqodri3@gmail.com>
2024-03-10 01:40:16 +0700

    Create Menu Utama dan Menu MathOne: Penjumlahan dan Pengurangan

---
+++ lib/views/MathAppOne.dart
@@ -0,0 +1,110 @@
+// ignore_for_file: prefer_const_constructors, library_private_types_in_public_api
+
+import 'package:flutter/material.dart';
+import 'package:tugastpm1_123190080_simpleapp/views/SecondScreen.dart';
+
+void main() {
+  runApp(MathAppOne());
+}
+
+class MathAppOne extends StatelessWidget {
+  MathAppOne({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Menu Penjumlahan dan Pengurangan',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MathPageOne(),
+    );
+  }
+}
+
+class MathPageOne extends StatefulWidget {
+  @override
+  _MathPageOneState createState() => _MathPageOneState();
+}
+
+class _MathPageOneState extends State<MathPageOne> {
+  TextEditingController num1Controller = TextEditingController();
+  TextEditingController num2Controller = TextEditingController();
+
+  int result = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) {
+                return SecondScreen();
+              }),
+            );
+          },
+        ),
+        title: Text('Menu Penjumlahan dan Pengurangan'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: num1Controller,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Angka Pertama',
+              ),
+            ),
+            TextField(
+              controller: num2Controller,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Angka Kedua',
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                int num1 = int.parse(num1Controller.text);
+                int num2 = int.parse(num2Controller.text);
+                setState(() {
+                  result = num1 + num2;
+                });
+              },
+              child: Text('Penjumlahan'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                int num1 = int.parse(num1Controller.text);
+                int num2 = int.parse(num2Controller.text);
+                setState(() {
+                  result = num1 - num2;
+                });
+              },
+              child: Text('Pengurangan'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Hasil: $result',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/SecondScreen.dart
@@ -0,0 +1,102 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:tugastpm1_123190080_simpleapp/views/MathAppOne.dart';
+
+class SecondScreen extends StatelessWidget {
+  SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Menu Utama'),
+        ),
+        body: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+                color: Colors.greenAccent,
+                //height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Column(children: [
+                  Container(
+                      color: Colors.greenAccent,
+                      height: 250,
+                      width: MediaQuery.of(context).size.width,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Welcome to Menu Utama",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            "Nama : Hazlan Muhammad Qodri",
+                            style: TextStyle(fontSize: 13),
+                          ),
+                          Text(
+                            "Nama : NIM: 123190080",
+                            style: TextStyle(fontSize: 13),
+                          ),
+                          Text(
+                            "Nama : Kelas: TPM IF-C",
+                            style: TextStyle(fontSize: 13),
+                          ),
+                          Text(
+                            "Dosen Pengampu : Bagus Muhammad Akbar, S.ST., M.Kom.",
+                            style: TextStyle(fontSize: 13),
+                          ),
+                        ],
+                      )),
+                  Container(
+                    color: Colors.white,
+                    height: 450,
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(
+                          "Pilih Menu",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          "Menu Penjumlahan dan Pengurangan",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        SizedBox(height: 3),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) {
+                                return MathAppOne();
+                              }),
+                            );
+                          },
+                          child: Text("+ and -"),
+                        ),
+                        SizedBox(height: 30),
+                        Text(
+                          "Menu Ganjil Genap",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        SizedBox(height: 3),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Text("odd and even"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ]))),
+      ),
+    );
+  }
+}
